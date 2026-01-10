5947492db019024a557af2be60f7b3bcb2044d5c
Elliot <sarah.bach.h63852@mcast.edu.mt>
2026-01-10 13:53:31 +0100

    coin counter
    fixed coin counter and added gui

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,7 +1,30 @@
 using UnityEngine;
+using TMPro;
 
 public class CoinManager : MonoBehaviour
 {
-    public int coinCount;
+    public static CoinManager instance;
+    public TMP_Text coinText;
+    public int coinCount = 0;
+    
+    
+
+    private void Awake()
+    {
+            instance = this; //  creates singleton instance
+
+    }
+
+    private void Start()
+    {
+        coinText.text = "Coins: " + coinCount.ToString(); // initializes coin text
+    }
+
+    public void IncreaseCoins(int v)
+    {
+        coinCount += v;
+        coinText.text = "Coins: " + coinCount.ToString(); // updates coin text
+    }
+
 
 }
